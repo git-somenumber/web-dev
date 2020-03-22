094c7b56eeeb29f4a8da693c4b601a48892df946
git-somenumber <51503822+git-somenumber@users.noreply.github.com>
2020-03-22 09:30:30 +0800

    Added eslint style guide and error guide

--- rich-text-editor/app.js
+++ rich-text-editor/app.js
@@ -1,65 +1,57 @@
-const cursor_p = document.getElementById('cursor');
-const italics_button = document.getElementById("italics");
-const bold_button = document.getElementById("bold");
-const underline_button = document.getElementById("underline");
-const editor_div = document.getElementById("editor");
+const cursorP = document.getElementById('cursor')
+// eslint-disable-next-line camelcase
+const italicsButton = document.getElementById('italics')
+const boldButton = document.getElementById('bold')
+const underlineButton = document.getElementById('underline')
+const editorDiv = document.getElementById('editor')
 
+// eslint-disable-next-line no-unused-vars
+var content = editorDiv.innerHTML
+const letters = new RegExp('[A-z]{1}| |[0-9]')
+const editor = new Editor()
 
-var content = editor_div.innerHTML;
-const letters = new RegExp('[A-z]{1}| |[0-9]');
-const editor = new Editor();
+console.log('working')
+var line = editor.newLine()
 
-console.log('working');
-var line = editor.newLine();
+editorDiv.addEventListener('keydown', function (event) {
+  if (event.keyCode === 37) {
+    console.log('Left')
+  } else if (event.keyCode === 39) {
+    console.log('Right')
+  } else if (event.key.match(RegExp('Shift|CapsLock|Alt|Meta'))) {
+  } else if (event.key.match(RegExp('Tab'))) {
 
-editor_div.addEventListener('keydown', function(event){  
-  if(event.keyCode == 37) {
-    console.log('Left');
-  }
-  else if(event.keyCode == 39) {
-    console.log('Right');
-  }
-  else if(event.key.match(RegExp('Shift|CapsLock|Alt'))){
-  }
-  else if(event.key.match(RegExp('Tab'))){
-    text_line += '    ';
-  }
-  else if(event.key.match(RegExp('Enter'))){
-    line.endLine();
-    line = editor.newLine();
-  }
-  else if((event.key.match(letters))){
-    line.add(event.key);
-  }
-  else{
+  } else if (event.key.match(RegExp('Enter'))) {
+    line.endLine()
+    line = editor.newLine()
+  } else if ((event.key.match(letters))) {
+    line.add(event.key)
+  } else {
     console.log(event)
   }
-  editor_div.innerHTML = editor.toHTML();
-});
+  content = editor.toHTML()
+})
 
-var boldStatus = false;
-bold_button.addEventListener('click',function(event){
-  if(!boldStatus){
-    console.log('Bold');
-    line.addBase('font-weight:bold;');
-    boldStatus = !boldStatus;
-  }else if(boldStatus){
-    line.escapeBase();
+var boldStatus = false
+boldButton.addEventListener('click', function (event) {
+  if (!boldStatus) {
+    console.log('Bold')
+    line.addBase('font-weight:bold;')
+    boldStatus = !boldStatus
+  } else if (boldStatus) {
+    line.escapeBase()
     console.log('unBold')
-    boldStatus = !boldStatus;
+    boldStatus = !boldStatus
   }
-  console.log(boldStatus);
-});
+  console.log(boldStatus)
+})
 
-italics_button.addEventListener('click',function(event){
-  console.log('Italics');
+italicsButton.addEventListener('click', function (event) {
+  console.log('Italics')
   line.addStyle('font-style:italic;')
-});
+})
 
-underline_button.addEventListener('click',function(event){
-  line.addStyle('text-decoration: underline;');
-  console.log('Underline');
-});
-
-
-
+underlineButton.addEventListener('click', function (event) {
+  line.addStyle('text-decoration: underline;')
+  console.log('Underline')
+})
--- rich-text-editor/objects.js
+++ rich-text-editor/objects.js
@@ -1,171 +1,171 @@
-class Base {
-    /**
-     * @param {string} style 
-     */
-    constructor(style) {
-        this.contents = "";
-        this.styles = style;
-        this.html = `<span style="{style-here}">{text}</span>`;
-        this.currentHtml = this.html.replace(new RegExp("{text}"), style);
-    }
+const textRegex = new RegExp('{text}')
+const styleRegex = new RegExp('{style-here}')
 
-    /**
+class Base {
+  /**
+     * @param {string} style
+     */
+  constructor (style) {
+    this.contents = ''
+    this.styles = style
+    this.html = '<span style="{style-here}" id="{id-here}" class="{class-here}">{text}</span>'
+    this.currentHtml = this.html.replace(textRegex, style)
+  }
+
+  /**
      * @param {string} char
      */
-    add(char) {
-        const textRegex = new RegExp("{text}");
-        this.contents += char;
-        this.currentHtml = this.html.replace(textRegex, this.contents);
-    }
+  add (char) {
+    this.contents += char
+    this.currentHtml = this.html.replace(textRegex, this.contents)
+  }
 
-    /**
-     * @param {string} styles 
+  /**
+     * @param {string} styles
      */
-    addStyle(styles) {
-        this.styles += styles;
-        var styleRegex = new RegExp("{style-here}");
-        this.currentHtml = this.html.replace(styleRegex, this.styles);
-    }
+  addStyle (styles) {
+    this.styles += styles
+    this.currentHtml = this.html.replace(styleRegex, this.styles)
+  }
 
-    /**
-     * 
-     * @param {string} style 
+  /**
+     *
+     * @param {string} style
      */
-    resetStyle(style) {
-        this.styles = style;
-    }
+  resetStyle (style) {
+    this.styles = style
+  }
 
-    /**
+  /**
      * @returns {string} Rendered Content
      */
-    render(){
-        return this.currentHtml.replace(new RegExp("{style-here}"), this.styles);
-    }
-
+  render () {
+    return this.currentHtml.replace(styleRegex, this.styles)
+  }
 }
 
 /**
- * 
+ *
  */
 class Line {
-    /**
-     * 
-     * @param {int} line 
-     * @param {string} style 
+  /**
+     *
+     * @param {int} line
+     * @param {string} style
      */
-    constructor(line, style = '') {
-        this.line = line;
-        this.bases = [];
-        this.bases[0] = new Base(style);
-        this.html = `<span style="{style-here}">{text}</span>`;
-        this.currentBase = 0;
-        this.end = false;
-    }
+  constructor (line, style = '') {
+    this.line = line
+    this.bases = []
+    this.bases[0] = new Base(style)
+    this.html = '<span style="{style-here}" id="{id-here}" class="{class-here}">{text}</span>'
+    this.currentBase = 0
+    this.end = false
+  }
 
-    /**
-     * 
+  /**
+     *
      * @param {string} character to be added
      */
-    add(ch){
-        this.bases[this.currentBase].add(ch)
-    }
+  add (ch) {
+    this.bases[this.currentBase].add(ch)
+  }
 
-    /**
+  /**
      * Ends the line
      */
-    endLine(){
-        this.end = true;
-    }
+  endLine () {
+    this.end = true
+  }
 
-    /**
+  /**
      * Adds a new line to the editor
      */
-    newLine(){
-        return new Line(this.line+1,this.bases[this.currentBase].styles);
-    }
+  newLine () {
+    return new Line(this.line + 1, this.bases[this.currentBase].styles)
+  }
 
-    /**
+  /**
      * Adds style to the line
-     * @param {string} styles 
+     * @param {string} styles
      */
-    addStyle(styles) {
-        this.bases[this.currentBase].addStyle(styles)
-    }
+  addStyle (styles) {
+    this.bases[this.currentBase].addStyle(styles)
+  }
 
-    /**
+  /**
      * Replaces the style for the line
-     * @param {string} style 
+     * @param {string} style
      */
-    resetStyle(style) {
-        this.bases[this.currentBase].resetStyle(style);
-    }
+  resetStyle (style) {
+    this.bases[this.currentBase].resetStyle(style)
+  }
 
-    /**
+  /**
      * (Helper)
      * Returns html for the line
      * @returns {string} HTML to be put into DOM
      */
-    toHTML(){
-        let currentHtml = ''
-        this.bases.forEach(base => {
-            currentHtml += base.render();
-        });
+  toHTML () {
+    let currentHtml = ''
+    this.bases.forEach(base => {
+      currentHtml += base.render()
+    })
 
-        if(this.end){
-            currentHtml += '<br>';
-        }
-
-        return this.html.replace(new RegExp("{text}"),currentHtml);
+    if (this.end) {
+      currentHtml += '<br>'
     }
 
-    /**
+    return this.html.replace(textRegex, currentHtml)
+  }
+
+  /**
      * Creates a new base for the styles to
      * be added in between line.
-     * @param {string} style 
+     * @param {string} style
      */
-    addBase(style = ''){
-        this.bases.push(new Base(style));
-        this.currentBase++;
-    }
+  addBase (style = '') {
+    this.bases.push(new Base(style))
+    this.currentBase++
+  }
 
-    /**
-     * Leaves a base so that we may continue 
+  /**
+     * Leaves a base so that we may continue
      * with default style for the line.
      */
-    escapeBase(){
-        this.currentBase--;
-    }
+  escapeBase () {
+    this.currentBase--
+  }
 }
 
 class Editor {
-    constructor(){
-        this.lines = [];
-        this.lineNum = 0;
-    }
+  constructor () {
+    this.lines = []
+    this.lineNum = 0
+  }
 
-    /**
+  /**
      * Adds a line to the editor
-     * @param {int} line 
+     * @param {int} line
      */
-    addLine(line){
-        this.lines[this.lineNum] = line;
-        this.lineNum++;
-    }
+  addLine (line) {
+    this.lines[this.lineNum] = line
+    this.lineNum++
+  }
 
-    newLine(){
-        let line = new Line(this.lineNum,'');
-        this.addLine(line);
-        return line;
-    }
+  newLine () {
+    const line = new Line(this.lineNum, '')
+    this.addLine(line)
+    return line
+  }
 
-    /**
+  /**
      * Renders the final html to be put into DOM
      */
-    toHTML(){
-        var html = "";
-        this.lines.forEach(line => {
-            html += line.toHTML();
-        });
-        return html;
-    }
+  toHTML () {
+    var html = ''
+    this.lines.forEach(line => {
+      html += line.toHTML()
+    })
+    return html
+  }
 }

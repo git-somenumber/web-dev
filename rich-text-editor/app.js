b364fd477922b6cf0ff3011fd1a9d86ca5a0eb47
git-somenumber <51503822+git-somenumber@users.noreply.github.com>
2020-03-30 18:52:40 +0800

    convert to TypeScript

--- rich-text-editor/app.js
+++ rich-text-editor/app.js
@@ -1,67 +1,66 @@
-const cursorP = document.getElementById('cursor')
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var objects_1 = require("./objects");
+var cursorP = document.getElementById('cursor');
 // eslint-disable-next-line camelcase
-const italicsButton = document.getElementById('italics')
-const boldButton = document.getElementById('bold')
-const underlineButton = document.getElementById('underline')
-const editorDiv = document.getElementById('editor')
-
+var italicsButton = document.getElementById('italics');
+var boldButton = document.getElementById('bold');
+var underlineButton = document.getElementById('underline');
+var editorDiv = document.getElementById('editor');
 // eslint-disable-next-line no-unused-vars
-var content = editorDiv.innerHTML
-const letters = new RegExp('[A-z]{1}| |[0-9]')
-const editor = new Editor()
-
-console.log('working')
-var line = editor.newLine()
-
+var content = editorDiv.innerHTML;
+var letters = new RegExp('[A-z]{1}| |[0-9]');
+var editor = new objects_1.Editor();
+console.log('working');
+var line = editor.newLine();
 editorDiv.addEventListener('keydown', function (event) {
-  if (event.keyCode === 37) {
-    console.log('Left')
-  } else if (event.keyCode === 39) {
-    console.log('Right')
-  } else if (event.key.match(RegExp('Shift|CapsLock|Alt|Meta'))) {
-  } else if (event.key.match(RegExp('Tab'))) {
-
-  } else if (event.key.match(RegExp('Enter'))) {
-    line.endLine()
-    line = editor.newLine()
-  } else if ((event.key.match(letters))) {
-    line.add(event.key)
-  } else {
-    console.log(event)
-  }
-  editorDiv.innerHTML = editor.toHTML() + '<span id="cursor">|</span>'
-})
-
-var boldStatus = false
+    if (event.keyCode === 37) {
+        console.log('Left');
+    }
+    else if (event.keyCode === 39) {
+        console.log('Right');
+    }
+    else if (event.key.match(RegExp('Shift|CapsLock|Alt|Meta'))) {
+    }
+    else if (event.key.match(RegExp('Tab'))) {
+    }
+    else if (event.key.match(RegExp('Enter'))) {
+        line.endLine();
+        line = editor.newLine();
+    }
+    else if ((event.key.match(letters))) {
+        line.add(event.key);
+    }
+    else {
+        console.log(event);
+    }
+    editorDiv.innerHTML = editor.toHTML() + '<span id="cursor">|</span>';
+});
+var boldStatus = false;
 boldButton.addEventListener('click', function (event) {
-  styleEditor('font-weight:bold;', boldStatus)
-  boldStatus = !boldStatus
-  console.log(boldStatus)
-})
-
-var italicsStatus = false
+    styleEditor('font-weight:bold;', boldStatus);
+    boldStatus = !boldStatus;
+    console.log(boldStatus);
+});
+var italicsStatus = false;
 italicsButton.addEventListener('click', function (event) {
-  styleEditor('font-style:italic;', italicsStatus)
-  console.log(italicsStatus)
-  italicsStatus = !italicsStatus
-})
-
-var underlineStatus = false
+    styleEditor('font-style:italic;', italicsStatus);
+    console.log(italicsStatus);
+    italicsStatus = !italicsStatus;
+});
+var underlineStatus = false;
 underlineButton.addEventListener('click', function (event) {
-  console.log('Underline')
-
-  styleEditor('text-decoration: underline;', underlineStatus)
-  underlineStatus = !underlineStatus
-})
-
-function styleEditor (style, status) {
-  if (!status) {
-    console.log(style)
-
-    line.addBase(style)
-  } else if (status) {
-    console.log('Un ' + style)
-
-    line.addBase()
-  }
+    console.log('Underline');
+    styleEditor('text-decoration: underline;', underlineStatus);
+    underlineStatus = !underlineStatus;
+});
+function styleEditor(style, status) {
+    if (!status) {
+        console.log(style);
+        line.addBase(style);
+    }
+    else if (status) {
+        console.log('Un ' + style);
+        line.addBase();
+    }
 }
--- rich-text-editor/objects.js
+++ rich-text-editor/objects.js
@@ -1,175 +1,202 @@
-const textRegex = new RegExp('{text}')
-const styleRegex = new RegExp('{style-here}')
-
-class Base {
-  /**
-     * @param {string} style
-     */
-  constructor (style) {
-    this.contents = ''
-    this.styles = style
-    this.html = '<span style="{style-here}" id="{id-here}" class="{class-here}">{text}</span>'
-    this.currentHtml = this.html.replace(styleRegex, style)
-    this.currentHtml = this.currentHtml.replace(textRegex, this.contents)
-  }
-
-  /**
-     * @param {string} char
-     */
-  add (char) {
-    this.contents += char
-    this.currentHtml = this.html.replace(textRegex, this.contents)
-  }
-
-  /**
-     * @param {string} styles
-     */
-  addStyle (styles) {
-    this.styles += styles
-    this.currentHtml = this.html.replace(styleRegex, this.styles)
-    console.log(this.currentHtml + 'style Log')
-  }
-
-  /**
-     *
-     * @param {string} style
-     */
-  resetStyle (style) {
-    this.styles = style
-  }
-
-  /**
-     * @returns {string} Rendered Content
-     */
-  render () {
-    const h = this.currentHtml.replace(styleRegex, this.styles)
-    console.log(h)
-    return this.currentHtml.replace(styleRegex, this.styles)
-  }
-}
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var textRegex = new RegExp('{text}');
+var styleRegex = new RegExp('{style-here}');
+var Base = /** @class */ (function () {
+    /**
+       * @param {string} style
+       */
+    function Base(style) {
+        this.contents = '';
+        this.styles = style;
+        this.html = '<span style="{style-here}" id="{id-here}" class="{class-here}">{text}</span>';
+        this.currentHtml = this.html.replace(styleRegex, style);
+        this.currentHtml = this.currentHtml.replace(textRegex, this.contents);
+    }
+    /**
+       * @param {string} char
+       */
+    Base.prototype.add = function (char) {
+        this.contents += char;
+        this.currentHtml = this.html.replace(textRegex, this.contents);
+    };
+    /**
+       * @param {string} styles
+       */
+    Base.prototype.addStyle = function (styles) {
+        this.styles += styles;
+        this.currentHtml = this.html.replace(styleRegex, this.styles);
+        console.log(this.currentHtml + 'style Log');
+    };
+    /**
+       *
+       * @param {string} style
+       */
+    Base.prototype.resetStyle = function (style) {
+        this.styles = style;
+    };
+    /**
+       * @returns {string} Rendered Content
+       */
+    Base.prototype.render = function () {
+        var h = this.currentHtml.replace(styleRegex, this.styles);
+        console.log(h);
+        return this.currentHtml.replace(styleRegex, this.styles);
+    };
+    return Base;
+}());
+exports.Base = Base;
 /**
  *
  */
-class Line {
-  /**
-     *
-     * @param {int} line
-     * @param {string} style
-     */
-  constructor (line, style = '') {
-    this.line = line
-    this.bases = []
-    this.bases[0] = new Base(style)
-    this.html = '<span style="{style-here}" id="{id-here}" class="{class-here}">{text}</span>'
-    this.currentBase = 0
-    this.end = false
-  }
-
-  /**
-     *
-     * @param {string} character to be added
-     */
-  add (ch) {
-    this.bases[this.currentBase].add(ch)
-  }
-
-  /**
-     * Ends the line
-     */
-  endLine () {
-    this.end = true
-  }
-
-  /**
-     * Adds a new line to the editor
-     */
-  newLine () {
-    return new Line(this.line + 1, this.bases[this.currentBase].styles)
-  }
-
-  /**
-     * Adds style to the line
-     * @param {string} styles
-     */
-  addStyle (styles) {
-    this.bases[this.currentBase].addStyle(styles)
-  }
-
-  /**
-     * Replaces the style for the line
-     * @param {string} style
-     */
-  resetStyle (style) {
-    this.bases[this.currentBase].resetStyle(style)
-  }
-
-  /**
-     * (Helper)
-     * Returns html for the line
-     * @returns {string} HTML to be put into DOM
-     */
-  toHTML () {
-    let currentHtml = ''
-    this.bases.forEach(base => {
-      currentHtml += base.render()
-    })
-
-    if (this.end) {
-      currentHtml += '<br>'
+var Line = /** @class */ (function () {
+    /**
+       *
+       * @param {number} line
+       * @param {string} style
+       */
+    function Line(line, style) {
+        if (style === void 0) { style = ''; }
+        this.line = line;
+        this.bases = [];
+        this.bases[0] = new Base(style);
+        this.html = '<span style="{style-here}" id="{id-here}" class="{class-here}">{text}</span>';
+        this.currentBase = 0;
+        this.end = false;
     }
-    console.log(currentHtml)
-    return this.html.replace(textRegex, currentHtml)
-  }
-
-  /**
-     * Creates a new base for the styles to
-     * be added in between line.
-     * @param {string} style
-     */
-  addBase (style = '') {
-    this.bases.push(new Base(style))
-    this.currentBase++
-  }
-
-  /**
-     * Leaves a base so that we may continue
-     * with default style for the line.
-     */
-  escapeBase () {
-    this.currentBase--
-  }
-}
-
-class Editor {
-  constructor () {
-    this.lines = []
-    this.lineNum = 0
-  }
-
-  /**
-     * Adds a line to the editor
-     * @param {int} line
-     */
-  addLine (line) {
-    this.lines[this.lineNum] = line
-    this.lineNum++
-  }
-
-  newLine () {
-    const line = new Line(this.lineNum, '')
-    this.addLine(line)
-    return line
-  }
-
-  /**
-     * Renders the final html to be put into DOM
-     */
-  toHTML () {
-    var html = ''
-    this.lines.forEach(line => {
-      html += line.toHTML()
-    })
-    return html
-  }
-}
+    /**
+       *
+       * @param {string} character to be added
+       */
+    Line.prototype.add = function (ch) {
+        this.bases[this.currentBase].add(ch);
+    };
+    /**
+       * Ends the line
+       */
+    Line.prototype.endLine = function () {
+        this.end = true;
+    };
+    /**
+       * Adds a new line to the editor
+       */
+    Line.prototype.newLine = function () {
+        return new Line(this.line + 1, this.bases[this.currentBase].styles);
+    };
+    /**
+       * Adds style to the line
+       * @param {string} styles
+       */
+    Line.prototype.addStyle = function (styles) {
+        this.bases[this.currentBase].addStyle(styles);
+    };
+    /**
+       * Replaces the style for the line
+       * @param {string} style
+       */
+    Line.prototype.resetStyle = function (style) {
+        this.bases[this.currentBase].resetStyle(style);
+    };
+    /**
+       * (Helper)
+       * Returns html for the line
+       * @returns {string} HTML to be put into DOM
+       */
+    Line.prototype.toHTML = function () {
+        var currentHtml = '';
+        this.bases.forEach(function (base) {
+            currentHtml += base.render();
+        });
+        if (this.end) {
+            currentHtml += '<br>';
+        }
+        console.log(currentHtml);
+        return this.html.replace(textRegex, currentHtml);
+    };
+    /**
+       * Creates a new base for the styles to
+       * be added in between line.
+       * @param {string} style
+       */
+    Line.prototype.addBase = function (style) {
+        if (style === void 0) { style = ''; }
+        this.bases.push(new Base(style));
+        this.currentBase++;
+    };
+    /**
+       * Leaves a base so that we may continue
+       * with default style for the line.
+       */
+    Line.prototype.escapeBase = function () {
+        this.currentBase--;
+    };
+    return Line;
+}());
+exports.Line = Line;
+var Editor = /** @class */ (function () {
+    function Editor() {
+        this.lines = [];
+        this.lineNum = 0;
+    }
+    /**
+       * Adds a line to the editor
+       * @param {Line} line
+       */
+    Editor.prototype.addLine = function (line) {
+        this.lines[this.lineNum] = line;
+        this.lineNum++;
+    };
+    Editor.prototype.newLine = function () {
+        var line = new Line(this.lineNum, '');
+        this.addLine(line);
+        return line;
+    };
+    /**
+       * Renders the final html to be put into DOM
+       */
+    Editor.prototype.toHTML = function () {
+        var html = '';
+        this.lines.forEach(function (line) {
+            html += line.toHTML();
+        });
+        return html;
+    };
+    return Editor;
+}());
+exports.Editor = Editor;
+var List = /** @class */ (function () {
+    function List(type) {
+        if (type === void 0) { type = 'O'; }
+        var Ohtml = '<ol>{items}</ol>';
+        var Uhtml = '<ul>{items}</ul>';
+        var itemHTML = '<li>{item}</li>';
+        this.refHtml = {
+            O: Ohtml,
+            U: Uhtml,
+            item: itemHTML
+        };
+        if (type === 'O') {
+            this.html = this.refHtml.O;
+        }
+        else if (type === 'U') {
+            this.html = this.refHtml.U;
+        }
+        this.items = []; //  Text Only
+        this.currentItem = 0;
+    }
+    List.prototype.newItem = function () {
+        this.currentItem++;
+    };
+    List.prototype.render = function () {
+        var _this = this;
+        var finalHtml = '';
+        this.items.forEach(function (item) {
+            finalHtml += _this.refHtml.item.replace(new RegExp('{item}'), item);
+        });
+        return this.currentHtml.replace(new RegExp('{items}'), finalHtml);
+    };
+    List.prototype.add = function (ch) {
+    };
+    return List;
+}());
+exports.List = List;
